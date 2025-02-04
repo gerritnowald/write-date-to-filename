b83ec6d5a17acbad2f01a3db5691024c35d74791
Gerrit Nowald <gerrit.nowald@3ds.com>
2025-02-04 13:29:27 +0100

    Add command-line argument to specify date for filename

--- filename_date_v1.0.py
+++ filename_date_v1.0.py
@@ -1,5 +1,10 @@
 import os
 import datetime
+import argparse
+
+argparser = argparse.ArgumentParser(description='Writes creation date into the filename')
+argparser.add_argument("-d", "--date", help="Specify the date in the filename, YYYY-MM-DD")
+args = argparser.parse_args()
 
 # Get the name of the current script
 current_script = os.path.basename(__file__)[:-3]
@@ -15,9 +20,13 @@ for file in files:
         # filename already has a date
         datetime.datetime.strptime(file[:10], '%Y-%m-%d')
     except:
-        # get the creation date of the file
-        creation_time = os.path.getctime(file)
-        creation_date = datetime.datetime.fromtimestamp(creation_time)
+        if args.date:
+            # date specified by the user
+            os.rename(file, args.date + '_' + file)
+        else:
+            # get the creation date of the file
+            creation_time = os.path.getctime(file)
+            creation_date = datetime.datetime.fromtimestamp(creation_time)
 
-        # rename the file
-        os.rename(file, str(creation_date)[:10] + '_' + file)
+            # rename the file
+            os.rename(file, str(creation_date)[:10] + '_' + file)
